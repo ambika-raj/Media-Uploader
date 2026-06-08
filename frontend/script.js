e2da8d41591810f0e4a402de14e76e08de7a06e6
Ambika Raj <ambikaraj2114@gmail.com>
2026-06-08 14:21:32 +0530

    changed backend live url in script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://media-uploader-imby.onrender.com";
 
 // ─────────────────────────────────────────────
 // 🎯 Grab all DOM elements we need
